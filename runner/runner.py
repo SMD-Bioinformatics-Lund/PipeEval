88b6a8dee60d0b0b9dc7fc3da0b509d17023a53e
Jakob Willforss <jakob.willforss@hotmail.com>
2024-11-13 09:28:45 +0100

    Single quote the pipeline call

--- runner/runner.py
+++ runner/runner.py
@@ -336,7 +336,7 @@ def build_start_nextflow_analysis_cmd(
         "--container",
         "/fs1/resources/containers/wgs_active.sif",
         "--pipeline",
-        "/fs1/pipelines/pipeeval/repos/cmd_wgs/main.nf -profile wgs",
+        "'/fs1/pipelines/pipeeval/repos/cmd_wgs/main.nf -profile wgs'",
     ]
     if stub_run:
         start_nextflow_command.append("--custom_flags")
