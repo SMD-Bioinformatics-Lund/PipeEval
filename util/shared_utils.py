29284e3eebda27ce4b576ebfbd10363f4bd8594e
Jakob Willforss <jakob.willforss@hotmail.com>
2024-11-14 10:08:47 +0100

    Prettify rows independent of type

--- util/shared_utils.py
+++ util/shared_utils.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, List, Optional
 
 
 def load_config(parent_path: str, config_path: Optional[str]) -> ConfigParser:
@@ -12,19 +12,21 @@ def load_config(parent_path: str, config_path: Optional[str]) -> ConfigParser:
     return config
 
 
-def prettify_rows(rows: List[List[str]], padding: int = 4) -> List[str]:
+def prettify_rows(rows: List[List[Any]], padding: int = 4) -> List[str]:
     column_widths = []
     for col_i in range(len(rows[0])):
         max_len = 0
         for row in rows:
-            cell_length = len(row[col_i])
+            cell_length = len(str(row[col_i]))
             if cell_length > max_len:
                 max_len = cell_length
         column_widths.append(max_len + padding)
 
     pretty_rows = []
     for row in rows:
-        adjusted_cells = [cell.ljust(column_widths[i]) for (i, cell) in enumerate(row)]
+        adjusted_cells = [
+            str(cell).ljust(column_widths[i]) for (i, cell) in enumerate(row)
+        ]
         adjusted_row = "".join(adjusted_cells)
         pretty_rows.append(adjusted_row)
     return pretty_rows
