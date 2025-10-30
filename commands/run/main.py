cb4fe10c2c5e70cf642074425ae27f69e25cac3c
Jakob Willforss <jakob.willforss@hotmail.com>
2025-10-30 10:46:12 +0100

    Update help text for --label

--- commands/run/main.py
+++ commands/run/main.py
@@ -367,7 +367,7 @@ def main_wrapper(args: argparse.Namespace):
 
 
 def add_arguments(parser: argparse.ArgumentParser):
-    parser.add_argument("--label", help="Something for you to use to remember the run")
+    parser.add_argument("--label", help="Optional custom label that will be part of the run label and thus part of the results folder name")
     parser.add_argument(
         "--checkout",
         required=True,
